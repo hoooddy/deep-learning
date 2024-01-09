e0461925e877dee2204d49acb2f88c6634d83ac3
kim980316@naver.com <kim980316@naver.com>
2024-01-10 00:36:02 +0900

    add - activation_function.py -- sigmoid

--- activation_function/activation_function.py
+++ activation_function/activation_function.py
@@ -10,7 +10,13 @@ def step_function(input_signal):
     return np.array(input_signal > 0, dtype=np.int32)
 
 
+def sigmoid(input_signal):
+    return 1 / (1 + np.exp(-input_signal))
+
+
 if __name__ == "__main__":
     print(step_function(input_signal=-0.1))
     print(step_function(input_signal=0.1))
 
+    print(sigmoid(input_signal=-0.1))
+    print(sigmoid(input_signal=1))
