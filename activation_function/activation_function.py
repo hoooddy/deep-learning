c2af1c5b67d88a097c728ae1f4f1116cee51fe31
kim980316@naver.com <kim980316@naver.com>
2024-01-11 20:26:01 +0900

    add - activation_function.py -- draw graph of step_function

--- activation_function/activation_function.py
+++ activation_function/activation_function.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pylab as plt
 
 
 def step_function(input_signal):
@@ -17,6 +18,10 @@ def sigmoid(input_signal):
 if __name__ == "__main__":
     print(step_function(input_signal=-0.1))
     print(step_function(input_signal=0.1))
+    x = np.arange(-5.0, 5.0, 0.1)
+    y = step_function(x)
+    plt.plot(x, y)
+    plt.show()
 
     print(sigmoid(input_signal=-0.1))
     print(sigmoid(input_signal=1))
