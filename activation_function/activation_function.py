f4229b456fdfa03205dd76b342dbf63bbcfd231b
kaaaile <kim980316@naver.com>
2024-01-17 22:18:29 +0900

    add - activation_function.py -- draw_graph

--- activation_function/activation_function.py
+++ activation_function/activation_function.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pylab as plt
+import os
 
 
 def step_function(a):
@@ -20,38 +21,37 @@ def tanh(a):
 
 
 def relu(a):
-    if a >= 0:
-        return 0
-    else:
-        return a
+    return np.maximum(0, a)
+
+
+def activation_function_example(activation_function, activation_function_title):
+    x_list = [-0.1, 0.1]
+    for a in x_list:
+        print(f"{activation_function_title}({a}) = {activation_function(a=a)}")
+
+
+def draw_graph(activation_function, graph_title):
+    x = np.arange(-5.0, 5.0, 0.1)
+    y = activation_function(a=x)
+
+    plt.title(graph_title)
+    plt.plot(x, y)
+
+    plt.savefig(f"graph/{graph_title}.png")
 
 
 if __name__ == "__main__":
-    print(step_function(a=-0.1))
-    print(step_function(a=0.1))
-    x = np.arange(-5.0, 5.0, 0.1)
-    y = step_function(x)
-    plt.plot(x, y)
-    plt.show()
+    if not os.path.exists("graph"):
+        os.makedirs("graph")
 
-    print(sigmoid(a=-0.1))
-    print(sigmoid(a=1))
-    x = np.arange(-5.0, 5.0, 0.1)
-    y = sigmoid(x)
-    plt.plot(x, y)
-    plt.show()
+    activation_function_example(step_function, "step_function")
+    draw_graph(step_function, "Step_Function")
 
-    print(tanh(a=-0.1))
-    print(tanh(a=1))
-    x = np.arange(-5.0, 5.0, 0.1)
-    y = tanh(x)
-    plt.plot(x, y)
-    plt.show()
+    activation_function_example(sigmoid, "sigmoid")
+    draw_graph(sigmoid, "Sigmoid")
 
-    print(relu(a=-0.1))
-    print(relu(a=1))
-    x = np.arange(-5.0, 5.0, 0.1)
-    y = relu(x)
-    plt.plot(x, y)
-    plt.show()
+    activation_function_example(tanh, "tanh")
+    draw_graph(tanh, "Tanh")
 
+    activation_function_example(relu, "relu")
+    draw_graph(relu, "ReLU")
