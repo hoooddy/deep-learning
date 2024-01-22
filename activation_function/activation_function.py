6ed326cf732cf38d63ca6952a6fc96afadaf3ad5
kaaaile <kim980316@naver.com>
2024-01-23 00:13:36 +0900

    add - activation_function_example.py

--- activation_function/activation_function.py
+++ activation_function/activation_function.py
@@ -1,6 +1,4 @@
 import numpy as np
-import matplotlib.pylab as plt
-import os
 
 
 def step_function(a):
@@ -24,34 +22,3 @@ def relu(a):
     return np.maximum(0, a)
 
 
-def activation_function_example(activation_function, activation_function_title):
-    x_list = [-0.1, 0.1]
-    for a in x_list:
-        print(f"{activation_function_title}({a}) = {activation_function(a=a)}")
-
-
-def draw_graph(activation_function, graph_title):
-    x = np.arange(-5.0, 5.0, 0.1)
-    y = activation_function(a=x)
-
-    plt.title(graph_title)
-    plt.plot(x, y)
-
-    plt.savefig(f"graph/{graph_title}.png")
-
-
-if __name__ == "__main__":
-    if not os.path.exists("graph"):
-        os.makedirs("graph")
-
-    activation_function_example(step_function, "step_function")
-    draw_graph(step_function, "Step_Function")
-
-    activation_function_example(sigmoid, "sigmoid")
-    draw_graph(sigmoid, "Sigmoid")
-
-    activation_function_example(tanh, "tanh")
-    draw_graph(tanh, "Tanh")
-
-    activation_function_example(relu, "relu")
-    draw_graph(relu, "ReLU")
